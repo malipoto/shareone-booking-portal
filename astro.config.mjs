854ddd4fa437ac3890435859a573228b878f9b2f
malipoto <d@share.one>
2026-02-09 12:23:28 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,7 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({});
+import vercel from '@astrojs/vercel';
+
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+});
